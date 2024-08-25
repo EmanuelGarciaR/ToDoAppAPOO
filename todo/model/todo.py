71713a37aba2abb1ce3c6c1b345fc78f04d5624c
Ema <emanuelgarciarios@gmail.com>
2024-08-24 21:13:29 -0500

    Create add_todo method

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -8,7 +8,7 @@ class TODO:
         self.tags = tags if tags is not None else []
 
     def __str__(self):
-        return f"TODO(code_id={self.code_id}, title='{self.title}', description='{self.description}', completed={self.completed}, tags={self.tags})"
+        return f"{self.code_id}-{self.title}"
 
     def mark_completed(self):
         self.completed = True
@@ -17,3 +17,18 @@ class TODO:
         if new_tag not in self.tags:
             self.tags.append(new_tag)
             # [new_tag if != tag for tag in self.tags]
+
+
+class TodoBook:
+    def __init__(self, todos: dict[int, TODO] = None):
+        self.todos = todos if todos is not None else {}
+
+    def add_todo(self, title: str, description: str) -> int:
+        new_id = len(self.todos) + 1
+
+        new_todo = TODO(code_id=new_id, title=title, description=description)
+
+        # Agregar el objeto TODO al diccionario 'todos' usando el ID como clave
+        self.todos[new_id] = new_todo
+
+        return new_id
