a0930c510ae39f73a6af35ebd7e0a90775040da8
Ema <emanuelgarciarios@gmail.com>
2024-08-24 20:51:52 -0500

    Create add_tag method

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -9,3 +9,11 @@ class TODO:
 
     def __str__(self):
         return f"TODO(code_id={self.code_id}, title='{self.title}', description='{self.description}', completed={self.completed}, tags={self.tags})"
+
+    def mark_completed(self):
+        self.completed = True
+
+    def add_tag(self, new_tag: str):
+        if new_tag not in self.tags:
+            self.tags.append(new_tag)
+            # [new_tag if != tag for tag in self.tags]
