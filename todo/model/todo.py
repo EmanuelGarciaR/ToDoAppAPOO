06221c03cff437f24055799bbd6560d02bfc979e
Ema <emanuelgarciarios@gmail.com>
2024-08-24 20:30:38 -0500

    Create TODO class

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -1,1 +1,11 @@
 # TODO: Add code here
+class TODO:
+    def __init__(self, code_id: int, title: str, description: str, completed: bool = False, tags: list[str] = None):
+        self.code_id = code_id
+        self.title = title
+        self.description = description
+        self.completed = completed
+        self.tags = tags if tags is not None else []
+
+    def __str__(self):
+        return f"TODO(code_id={self.code_id}, title='{self.title}', description='{self.description}', completed={self.completed}, tags={self.tags})"
